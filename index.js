24ad549b5c650b5f495042e296094d60b19febd5
Thilini Dharmawardhana <dhanu.thilini@gmail.com>
2023-08-31 00:03:14 -0500

    add more charators to the code by calling newItem function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //declaration of a funtion to display charactors
 function newImage(path,left,bottom){
-    let charactor=document.createElement('imgage')
+    let charactor=document.createElement('img')
     charactor.src=path
     charactor.style.position='fixed'
     charactor.style.left=left+'px'
@@ -16,7 +16,9 @@ function newImage(path,left,bottom){
 // greenCharacter.style.left = '100px'
 // greenCharacter.style.bottom = '100px'
 // document.body.append(greenCharacter)
-// //add a tree image to the page
+
+
+//add a tree image to the page
 // let Pinetree = document.createElement('img')
 // Pinetree.src = 'assets/pine-tree.png'
 // Pinetree.style.position = 'fixed'
@@ -31,3 +33,20 @@ newImage('assets/tree.png',200,300)
 newImage('assets/pillar.png',350,100)
 newImage('assets/crate.png',150,200)
 newImage('assets/well.png',500,425)
+
+//add sward iamge to the page
+let sword=document.createElement('img')
+sword.src='assets/sword.png'
+sword.position='fixed'
+sword.left='500px'
+sword.bottom='405px'
+document.body.append(sword)
+
+sword.addEventListener('click',function()
+{
+    sword.remove()
+})
+
+//add shield and staff images to the page
+newItem('assets/shield.png',165,185)
+newItem('assets/staff.png',600,100)
